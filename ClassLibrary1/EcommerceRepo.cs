47ce1ad9f39155422f768e3a73a07f7ffb416bfc
DESKTOP-8EBH9SK <mathias750@revature.net>
2022-08-27 13:42:10 -0500

    Database Setup

---
+++ ClassLibrary1/EcommerceRepo.cs
@@ -0,0 +1,25 @@
+﻿namespace RepoLayer
+{
+    public class EcommerceRepo
+    {
+        /* public async Task<List<Request>> RequestsAsync(int type)
+         {
+             SqlConnection conn = new SqlConnection("Server=tcp:mathiasriverasqlserver1.database.windows.net,1433;Initial Catalog=RainbowRoadP2;Persist Security Info=False;User ID=MathiasRiveraRevature1;Password=JohnDaniel(9);MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+             using (SqlCommand command = new SqlCommand($"SELECT * FROM Request WHERE Status = @type", conn))
+             {
+                 command.Parameters.AddWithValue("@type", type); //SQL inj prevention
+                 conn.Open();
+                 SqlDataReader? ret = await command.ExecuteReaderAsync();
+                 List<Request> rlist = new List<Request>();
+                 while (ret.Read())
+                 {
+                     Request r = new Request((Guid)ret[0], (Guid)ret[1], ret.GetString(2), ret.GetDecimal(3), ret.GetInt32(4));
+                     rlist.Add(r);
+                 }
+                 conn.Close();
+                 return rlist;
+             }
+
+         } */
+    }
+}
