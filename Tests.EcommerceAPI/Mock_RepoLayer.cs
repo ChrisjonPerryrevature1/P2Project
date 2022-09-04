f05350396f98d9ecd6671b700e2d8110e02fa552
DESKTOP-8EBH9SK <mathias750@revature.net>
2022-09-04 15:33:09 -0500

    started unit testing

---
+++ Tests.EcommerceAPI/Mock_RepoLayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RepoLayer;
+using BusinessLayer;
+using Models;
+using System.Data.SqlClient;
+
+namespace Tests.EcommerceAPI
+{
+    public class Mock_RepoLayer : IEcommerceRepo
+    {
+        public async Task<Customers> RegisterCustomerAsync(Customers customer)
+        {
+            await Task.Delay(100);
+            throw new NotImplementedException();
+        }
+        public async Task<Customers> LoginAsync(Customers login)
+        {
+            await Task.Delay(100);
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Inventory>> ViewInventoryAsync()
+        {
+            await Task.Delay(100);
+            throw new NotImplementedException();
+        }
+
+        public async Task<Cart> FillCartAsync(Cart cart)
+        {
+            await Task.Delay(100);
+            throw new NotImplementedException();
+        }
+
+        public async Task<Cart> EditCartAsync(Cart cart)
+        {
+            await Task.Delay(100);
+            throw new NotImplementedException();
+        }
+    }
+}
--- Tests.EcommerceAPI/UnitTest1.cs
+++ Tests.EcommerceAPI/UnitTest1.cs
@@ -1,11 +1,72 @@
-namespace Tests.EcommerceAPI
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace Tests.EcommerceAPI;
+using Models;
+using BusinessLayer;
 
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void CustomerIDdtoCreatedCorrectly()
+    {
+        //Arrange
+        int id = 100;
+        //Act
+        CustomerIDdto dto = new CustomerIDdto
+        {
+            CustomerID = id
+        };
+        //Assert
+        Assert.Equal(dto.CustomerID, id);
+    }
+
+    [Fact]
+    public void CustomersModelCheck()
+    {
+        //Arrange
+        string email = "1@g.com";
+        string password = "password123";
+        //Act
+        Customers customer = new Customers
+        {
+            Email = email,
+            Password = password
+        };
+        //Assert
+        Assert.Equal(password, customer.Password);
+
+    }
+
+    [Fact]
+    public void InventoryModelCheck()
+    {
+        //Arrange
+        int inventoryID = 100;
+        string name = "brick";
+        int quantity = 50;
+        decimal price = 1.00M;
+        //Assert
+        Inventory inv = new Inventory
+        {
+            ItemId = inventoryID,
+            ItemName = name,
+            Quantity = quantity,
+            Price = price
+        };
+        //Act
+        Assert.Equal(price, inv.Price);
+    }
+
+    [Fact]
+    public async void RegisterCustomerCheck()
+    {
+        //Arrange
+        Customers c = new Customers();
+        c.Email = "email@email.com";
+        c.Password = "password456";
+        Mock_RepoLayer m = new Mock_RepoLayer();
+        EcommerceBusiness eb = new EcommerceBusiness(m);
+        //Act
+        Customers result = await eb.RegisterCustomerAsync(c);
+        //Assert
+        Assert.Equal(c.Password, result.Password);
     }
 }
